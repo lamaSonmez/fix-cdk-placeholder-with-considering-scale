b1792bbbfb3e422082b36d03ad100e1f681fc919
Lama Sonmez <lamasonmez.maids.cc@gmail.com>
2022-02-15 13:04:43 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {CdkDragDropCustomPlaceholderExample} from './cdk-drag-drop-custom-placeholder-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [CdkDragDropCustomPlaceholderExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [CdkDragDropCustomPlaceholderExample],
+})
+export class AppModule {}
